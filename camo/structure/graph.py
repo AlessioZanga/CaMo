3ff20b4fd1ccb9671ddffb683254668b3f3ba81b
AlessioZanga <alessio.zanga@outlook.it>
2021-01-06 21:40:24 +0100

    Change path to list of tuples

--- camo/structure/graph.py
+++ camo/structure/graph.py
@@ -52,8 +52,8 @@ class Graph:
         subgraph = self._G.subgraph(V)
         return type(self)(subgraph.nodes, subgraph.edges)
     
-    def paths(self, u: str, v: str) -> List[List[str]]:
-        return list(nx.all_simple_paths(self._G, u, v))
+    def paths(self, u: str, v: str) -> List[Tuple[str]]:
+        return [tuple(p) for p in nx.all_simple_paths(self._G, u, v)]
     
     def plot(self) -> None:
         sty = {"node_shape": ""}
