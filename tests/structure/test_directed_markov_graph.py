c72d7be9d678ca4884a5987bbea3d217708188bd
AlessioZanga <alessio.zanga@outlook.it>
2021-01-08 13:18:52 +0100

    Removed unnecessary brackets

--- tests/structure/test_directed_markov_graph.py
+++ tests/structure/test_directed_markov_graph.py
@@ -3,23 +3,23 @@ from camo.data import primer
 
 def test_is_chain():
     model = primer.figure_2_1
-    assert(model.is_chain("X", "Y", "Z"))
+    assert model.is_chain("X", "Y", "Z")
 
 def test_is_fork():
     model = primer.figure_2_2
-    assert(model.is_fork("X", "Y", "Z"))
+    assert model.is_fork("X", "Y", "Z")
 
 def test_is_collider():
     model = primer.figure_2_3
-    assert(model.is_collider("X", "Y", "Z"))
+    assert model.is_collider("X", "Y", "Z")
 
 def test_d_separation():
     model = primer.figure_2_7
 
-    assert(model.is_d_separated("Y", "Z"))
-    assert(not model.is_d_separated("Y", "Z", "W"))
-    assert(not model.is_d_separated("Y", "Z", "U"))
-    assert(model.is_d_separated("Y", "Z", {"W", "X"}))
+    assert model.is_d_separated("Y", "Z")
+    assert not model.is_d_separated("Y", "Z", "W")
+    assert not model.is_d_separated("Y", "Z", "U")
+    assert model.is_d_separated("Y", "Z", {"W", "X"})
 
     model = primer.figure_2_8
 
@@ -35,10 +35,10 @@ def test_d_separation():
         {"W", "U", "X"}
     ]
 
-    assert(all([
+    assert all([
         not model.is_d_separated("Y", "Z", S)
         for S in dependent_sets
-    ]))
+    ])
 
     independent_sets = [
         "T",
@@ -48,7 +48,7 @@ def test_d_separation():
         {"W", "U", "X", "T"}
     ]
 
-    assert(all([
+    assert all([
         model.is_d_separated("Y", "Z", S)
         for S in independent_sets
-    ]))
+    ])
