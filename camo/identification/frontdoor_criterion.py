feaae1860d5cdbefece7fc75e2e52cf9db016e23
AlessioZanga <alessio.zanga@outlook.it>
2021-01-09 00:33:24 +0100

    Add frontdoor adjustment sets

--- camo/identification/frontdoor_criterion.py
+++ camo/identification/frontdoor_criterion.py
@@ -2,6 +2,7 @@ from typing import List, Set, Tuple
 
 from .backdoor_criterion import is_backdoor_adjustment_set
 from ..structure import SCM
+from ..utils import powerset
 
 
 def is_frontdoor_adjustment_set(model: SCM, X: str, Y: str, Z: str = None) -> bool:
@@ -24,3 +25,34 @@ def is_frontdoor_adjustment_set(model: SCM, X: str, Y: str, Z: str = None) -> bo
         return False
 
     return True
+
+
+def all_frontdoor_adjustment_sets(model: SCM, X: str, Y: str) -> List[Set[str]]:
+    adjustment_variables = model.endogenous_variables - {X, Y, *model.descendants(X)}
+
+    adjustment_sets = [
+        set(S)
+        for S in powerset(adjustment_variables)
+        if is_frontdoor_adjustment_set(model, X, Y, S)
+    ]
+
+    return adjustment_sets
+
+
+def minimal_frontdoor_adjustment_sets(model: SCM, X: str, Y: str) -> List[Set[str]]:
+    adjustment_variables = model.endogenous_variables - {X, Y, *model.descendants(X)}
+
+    adjustment_sets = []
+    for S in powerset(adjustment_variables):
+        S = set(S)
+        # An adjustment set is minimal if it is not
+        # a super set of any smaller adjustment set
+        is_super_set = any(S.issuperset(s) for s in adjustment_sets)
+        if not is_super_set and is_frontdoor_adjustment_set(model, X, Y, S):
+            adjustment_sets.append(S)
+
+    return adjustment_sets
+
+
+def frontdoor_paths(model: SCM, X: str, Y: str) -> List[Tuple[str]]:
+    raise NotImplementedError()
