1fbe4b5a881382fe537806bbaabb5069bea9a1ec
AlessioZanga <alessio.zanga@outlook.it>
2021-01-16 23:28:09 +0100

    Add CI comments

--- camo/backend/conditional_independence_test.py
+++ camo/backend/conditional_independence_test.py
@@ -21,10 +21,15 @@ CONDITIONAL_INDEPENDENCE_TESTS = {
 def _power_divergence(data: pd.DataFrame, X: str, Y: str, Z: Set[str] = None, method: str = None):
     Z = list(_as_set(Z))
 
+    # Group data by Z
     data = [data] if not Z else (d for _, d in data.groupby(Z))
+    # For each group compute the absolute frequency
     data = (d.groupby([X, Y]).size().unstack(Y, 0) for d in data)
+    # Apply the selected contional indepependece test
     data = (chi2_contingency(d, lambda_=method) for d in data)
+    # Reduce results among groups by using column-wise sum
     data = np.vstack([np.array(d[:3]) for d in data]).sum(axis=0)
+    # Recompute p_value using chi and dof values
     data[1] = 1 - chi2.cdf(data[0], df=data[2])
     return data
 
